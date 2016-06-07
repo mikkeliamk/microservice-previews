b95718ec8421f3ffb4fad1ac0818baf107cc400e
liisauosukainen <liisa.uosukainen@mamk.fi>
2016-06-07 10:53:57 +0300

    General improvements and bug fixes.

--- src/main/java/fi/mamk/osa/microservices/GeneratePreview.java
+++ src/main/java/fi/mamk/osa/microservices/GeneratePreview.java
@@ -13,6 +13,9 @@ import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Hashtable;
 
@@ -21,10 +24,9 @@ import net.xeoh.plugins.base.annotations.Capabilities;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
 import org.imgscalr.Scalr;
-import org.jpedal.PdfDecoder;
-import org.jpedal.exception.PdfException;
-import org.jpedal.fonts.FontMappings;
 
 @PluginImplementation
 public class GeneratePreview extends Microservice {
@@ -40,12 +42,13 @@ public class GeneratePreview extends Microservice {
     public boolean execute(String input, HashMap<String, Object> options)
             throws Exception {
         boolean success = false;
+        String state = "error";
         String output = "";
         String importDir = "";
         String uploadDir = "";
         String ingestDir = "";
+        String failedDir = "";
         String filename = "";
-        String format = "";
         String organization = "";
         String user = "";
         
@@ -59,11 +62,11 @@ public class GeneratePreview extends Microservice {
             if (options.containsKey("ingestdirectory")) {
                 ingestDir = options.get("ingestdirectory").toString();
             }
+            if (options.containsKey("faileddirectory")) {
+                failedDir = options.get("faileddirectory").toString();
+            }
             if (options.containsKey("filename")) {
                 filename = options.get("filename").toString();
-            }
-            if (options.containsKey("type")) {
-                format = options.get("type").toString();
             }
             if (options.containsKey("organization")) {
                 organization = options.get("organization").toString();
@@ -73,15 +76,37 @@ public class GeneratePreview extends Microservice {
             }
         }
 
-        String filePath = importDir + filename;
-        String thumbPath = uploadDir + filename;
-        output += createThumbFile(filePath, format, thumbPath);
+        String filePath = importDir;
+        String thumbPath = uploadDir;
         
-        // move to ingest directory
-        output += moveToIngestDirectory(filename, importDir, ingestDir, output);
+        try {
+                   
+            // create thumb
+            output += createThumbFile(filePath, thumbPath, filename);
 
-        success = true;
-        super.setState("completed");
+            // move to ingest directory
+            output += moveToIngestDirectory(filename, importDir, ingestDir);
+            
+            state = "completed";
+            success = true;
+       
+        } catch (Exception e) {
+            output += "Creating thumbnail for "+filename+" failed.\n"+e.toString()+"\n";
+        }
+        
+        if (!success) {
+            // if exception, move imported file to failed directory
+            File file = new File(importDir + filename);
+            File failedfile = findFileName(failedDir, filename);
+                        
+            if (!failedfile.getParentFile().exists()) {
+                failedfile.getParentFile().mkdirs();
+            }
+            
+            FileUtils.moveFile(file, failedfile);
+        }
+              
+        super.setState(state);
         super.setOutput(output);
         super.setCompleted(true);
         
@@ -89,7 +114,7 @@ public class GeneratePreview extends Microservice {
         super.setLog(log);
         log();
         
-        return success;    
+        return success;
     }
 
     /**
@@ -100,130 +125,161 @@ public class GeneratePreview extends Microservice {
      * @throws IOException
      * @throws PdfException
      */
-    private String createThumbFile(String filePath, String format, String thumbPath) {
-        String fileExtension                = filePath.substring(filePath.lastIndexOf('.'), filePath.length());
-        String fileName                     = filePath.substring(filePath.lastIndexOf('/')+1, filePath.lastIndexOf('.'));
-        PdfDecoder pdfDecoder               = null;
-        File pdfFile                        = null;
+    private String createThumbFile(String filePath, String thumbPath, String fileName) throws Exception, IOException {
+        String fileExtension                = FilenameUtils.getExtension(fileName);
+        String baseName                     = FilenameUtils.removeExtension(fileName);
+        File origFile                       = null;
         File thumbFile                      = null;
         BufferedImage bufferedImage         = null;
         BufferedImage thumb                 = null;
         String thumbnailPathAndName         = "";
         FileOutputStream fileOutputStream   = null;
         String output = "";
+               
+        String thumbnailName = THUMB_DSID + baseName + "."+THUMB_EXTENSION;
+              
+        thumbFile = new File(thumbPath + thumbnailName);
+        origFile = new File(filePath + fileName);
         
-        // use the same path, but change the extension to png
-        thumbnailPathAndName = FilenameUtils.removeExtension(thumbPath)+"."+THUMB_EXTENSION;
-        thumbnailPathAndName = thumbnailPathAndName.replace(fileName, THUMB_DSID+fileName);
-        
-        thumbFile = new File(thumbnailPathAndName);
-        pdfFile = new File(filePath);
-        if (thumbFile.exists() || !pdfFile.exists()) {
-            return output;
+        if (thumbFile.exists()) {
+            output += "Cannot create thumbnail for "+fileName+". File already exists.\n";
+            throw new Exception(output);
         }
         
-        if (fileExtension.equalsIgnoreCase(".pdf")) {
-            pdfDecoder = new PdfDecoder(true);
+        fileOutputStream = new FileOutputStream(thumbFile.toString()); 
+            
+        if (fileExtension.equalsIgnoreCase("pdf")) {
             try {
-                // set mappings for non-embedded fonts to use
-                FontMappings.setFontReplacements();
-        
                 // open the PDF file
-                pdfDecoder.openPdfFile(filePath);
-                /**get page 1 as an image*/
-                bufferedImage = pdfDecoder.getPageAsImage(1);
+                PDDocument document = PDDocument.load(origFile.toString());
+                                                
+                PDPage firstPage = (PDPage) document.getDocumentCatalog().getAllPages().get(0);
+                // get page 1 as an image
+                bufferedImage = firstPage.convertToImage();
                 thumb = Scalr.resize(bufferedImage, 250);
-                fileOutputStream = new FileOutputStream(thumbnailPathAndName);
+                
                 ImageIO.write(thumb, THUMB_EXTENSION, fileOutputStream);
+                
                 // close the pdf file
-                pdfDecoder.closePdfFile();            
+                document.close();
                 
             } catch (IOException e) {
-                output += "Error creating thumbnail for "+FilenameUtils.getName(filePath)+": "+e.getMessage()+"\n";
-            } catch (PdfException e) {
-                output += "Error creating thumbnail for "+FilenameUtils.getName(filePath)+": "+e.getMessage()+"\n";
+                output += "Error creating thumbnail for pdf file: "+e.getMessage();
+                throw new Exception(output);
             }
             
-        } else  if (fileExtension.equalsIgnoreCase(".tif")
-                    || fileExtension.equalsIgnoreCase(".jpg")
-                    || fileExtension.equalsIgnoreCase(".gif")
-                    || fileExtension.equalsIgnoreCase(".png")) {
+        } else  if (fileExtension.equalsIgnoreCase("tif")
+                    || fileExtension.equalsIgnoreCase("jpg")
+                    || fileExtension.equalsIgnoreCase("jpeg")
+                    || fileExtension.equalsIgnoreCase("jpf")
+                    || fileExtension.equalsIgnoreCase("jp2")
+                    || fileExtension.equalsIgnoreCase("gif")
+                    || fileExtension.equalsIgnoreCase("png")) {
             
-            try {
-                // Decide if or not try to make an animated gif thumb for animated gif files
-                // At the moment it does not and current solution (Scalr) does not support making animated gifs
-
-                if (fileExtension.equalsIgnoreCase(".tif")) {
-                    SeekableStream s = new FileSeekableStream(filePath);
-                    TIFFDecodeParam param = null;
-                    ImageDecoder dec = ImageCodec.createImageDecoder("tiff", s, param);
-                    RenderedImage op = dec.decodeAsRenderedImage(0);
-                    // RenderedImage to BufferedImage
-                    ColorModel cm = op.getColorModel();
-                    int width = op.getWidth();
-                    int height = op.getHeight();
-                    WritableRaster raster = cm.createCompatibleWritableRaster(width, height);
-                    boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
-                    Hashtable properties = new Hashtable();
-                    String[] keys = op.getPropertyNames();
-                    if (keys!=null) {
-                        for (int i = 0; i < keys.length; i++) {
-                            properties.put(keys[i], op.getProperty(keys[i]));
-                        }
+            // Decide if or not try to make an animated gif thumb for animated gif files
+            // At the moment it does not and current solution (Scalr) does not support making animated gifs
+            if (fileExtension.equalsIgnoreCase("tif")) {
+                SeekableStream s = new FileSeekableStream(origFile.toString());
+                TIFFDecodeParam param = null;
+                ImageDecoder dec = ImageCodec.createImageDecoder("tiff", s, param);
+                RenderedImage op = dec.decodeAsRenderedImage(0);
+                // RenderedImage to BufferedImage
+                ColorModel cm = op.getColorModel();
+                int width = op.getWidth();
+                int height = op.getHeight();
+                WritableRaster raster = cm.createCompatibleWritableRaster(width, height);
+                boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+                Hashtable properties = new Hashtable();
+                String[] keys = op.getPropertyNames();
+                if (keys!=null) {
+                    for (int i = 0; i < keys.length; i++) {
+                        properties.put(keys[i], op.getProperty(keys[i]));
                     }
-                    bufferedImage = new BufferedImage(cm, raster, isAlphaPremultiplied, properties);
-                    op.copyData(raster);
-                    
+                }
+                bufferedImage = new BufferedImage(cm, raster, isAlphaPremultiplied, properties);
+                op.copyData(raster);
+                
+                thumb = Scalr.resize(bufferedImage, 200); // Scale the original image around the width and height of 150 px, maintaining original aspect ratio
+                ImageIO.write(thumb, THUMB_EXTENSION, fileOutputStream);
+                
+            } else {
+                
+                bufferedImage = ImageIO.read(new File(origFile.toString()));
+                if (bufferedImage != null) {
                     thumb = Scalr.resize(bufferedImage, 200); // Scale the original image around the width and height of 150 px, maintaining original aspect ratio
-                    fileOutputStream = new FileOutputStream(thumbnailPathAndName);
                     ImageIO.write(thumb, THUMB_EXTENSION, fileOutputStream);
                     
                 } else {
-                    bufferedImage = ImageIO.read(new File(filePath));
-                    thumb = Scalr.resize(bufferedImage, 200); // Scale the original image around the width and height of 150 px, maintaining original aspect ratio
-                    fileOutputStream = new FileOutputStream(thumbnailPathAndName);
-                    ImageIO.write(thumb, THUMB_EXTENSION, fileOutputStream);
-
+                    output += "Cannot create thumbnail for "+fileName+"\n";
+                    throw new Exception(output);
                 }
-                
-            } catch (IOException e) {
-                output += "Error creating thumbnail for "+FilenameUtils.getName(filePath)+": "+e.getMessage()+"\n";
             }
-            
+                    
         } else {
-            output += "Cannot create thumbnail for "+FilenameUtils.getName(filePath)+"\n";
+            output += "Cannot create thumbnail for "+fileName+"\n";
+            throw new Exception(output);
         }
-        
+               
         if (thumbFile.canRead()) {
-            try {
-                fileOutputStream.close();
-            } catch (IOException e) {
-                output += "Error creating thumbnail file: "+e.getMessage()+"\n";
-            }
+            fileOutputStream.close();
+            output += "Preview generated for "+fileName+"\n";
+
+        } else {
+            output += "Error creating thumbnail file for "+fileName+"\n";
+            throw new Exception(output);
         }
-        
-        output += "Preview generated for "+FilenameUtils.getName(filePath)+"\n";
         
         return output;
     }
 
-    public String moveToIngestDirectory(String fileName, String importDirectory, String ingestDirectory, String output) {
-
+    private String moveToIngestDirectory(String fileName, String importDirectory, String ingestDirectory) throws Exception {
+        
         File file = new File(importDirectory + fileName);
-        File targetfile = new File(ingestDirectory + file.getName());
-
+        File targetfile = new File(ingestDirectory + fileName);
+        String output = "";
+        
         if (!targetfile.getParentFile().exists()) {
             targetfile.getParentFile().mkdirs();
         }
-
-        try {
+        
+        if (targetfile.exists()) {
+            output += "Cannot move "+fileName+". File already exists.\n";
+            throw new Exception(output);
+        }
+        
+        if (file.exists()) {
             FileUtils.moveFile(file, targetfile);
             output += "File "+fileName+" moved to ingestDirectory.\n";
-
-        } catch (IOException e) {
-            output += "moveToIngestDirectory error, "+e+"\n";
+            
         }
+        
         return output;
+    }
+    
+    /**
+     * findFileName
+     * @param dir              absolute path 
+     * @param fileName         fileName
+     * @return                 filename with the running nr of copies if file already exists in dir
+     */   
+    private File findFileName(String dir, String fileName) {
+        
+        File file = new File(dir + fileName);
+        if (!file.exists()) {
+            return file;
+        }
+               
+        String baseName = FilenameUtils.removeExtension(fileName);
+        String extension = FilenameUtils.getExtension(fileName);
+
+        for (int i = 1; i < Integer.MAX_VALUE; i++) {
+            Path path = Paths.get(dir, String.format("%s(%d).%s", baseName, i, extension));
+            if (!Files.exists(path)) {
+                return path.toFile();
+            }
+        }
+        
+        return file;
     }
 
 }
